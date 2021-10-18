8b11cb16b5a7bb969dddb67a3ab57190d4e15486
JesseLekwa <jesse.lekwa@gmail.com>
2021-10-17 23:51:08 -0400

    final commit

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -1,9 +1,33 @@
 package baseline;
 
-
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Solution42 {
-    
+
+    public void formatFile(String filename){
+        try {
+            // scan in file
+            File file = new File(filename);
+            Scanner sc = new Scanner(file);
+
+            //  loop to read and parse
+            while(sc.hasNext()) {
+                String s = sc.nextLine();
+                String[] temp = s.split(",");
+                //  output format
+                System.out.printf("%-10s%-10s%-10s%n", temp[0], temp[1], temp[2]);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error: File not found");
+        }
+
+    }
+    public static void main(String[] args){
+        Solution42 Employee = new Solution42();
+        System.out.printf("%-10s%-10s%-10s%n", "Last", "First", "Salary");
+        System.out.printf("---------------------------");
+        Employee.formatFile("exercise42_input.txt");
+    }
 }
---
+++ exercise42/src/test/java/baseline/Solution42test.java
@@ -0,0 +1,5 @@
+package baseline;
+
+public class Solution42test {
+    //test code
+}
